f948269eac59ed23bd8b4464e7d01c0b4e9af6fe
p.tamkovich <p.tamkovich@samsung.com>
2018-02-26 23:17:32 +0100

    Add project files.

---
+++ NetCoreIdentity/Models/ApplicationContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NetCoreIdentity.Models
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions options): base(options) {}
+    }
+}
